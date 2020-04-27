f488e2b77d20449be7553b7eb16fe0b7ca2c4188
Martins <martinstsuki@gmail.com>
2020-04-27 06:46:37 -0300

    Update ex002.c

--- c/ex002.c
+++ c/ex002.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void main() {
-    printf("--------------------\n");
-    printf("\\a\t=\tBeep\n");
-    printf("\\n\t=\tNova linha\n");
-    printf("\\t\t=\tTabulação\n");
-    printf("\\\t=\tBarra\n");
-    printf("%%%%\t=\tPorcentagem\n");
-    printf("\\?\t=\tInterrogação\n");
-    printf("--------------------\n");
+  printf("--------------------\n");
+  printf("\\a\t=\tBeep\n");
+  printf("\\n\t=\tNova linha\n");
+  printf("\\t\t=\tTabulação\n");
+  printf("\\\t=\tBarra\n");
+  printf("%%%%\t=\tPorcentagem\n");
+  printf("\\?\t=\tInterrogação\n");
+  printf("--------------------\n");
 }
