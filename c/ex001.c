07f63f6f3ed3e7027d7061c924d5088a03a051a2
Martins <martinstsuki@gmail.com>
2020-04-27 01:35:00 -0300

    Add ex001.c

---
+++ c/ex001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("\n Listagem de Alunos");
+    printf("\n Nome\t\tNota");
+    printf("\n--------------------");
+    printf("\nVictoria F...\t10.0");
+    printf("\nSakuya Martins\t10.0");
+    printf("\nRemi Martins\t10.0");
+    printf("\nYuu Finotti\t10.0\n\n");
+}
