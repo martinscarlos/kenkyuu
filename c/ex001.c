8b3aaf268581c5a8c41a8bc94c45028d8fce63ba
Martins <martinstsuki@gmail.com>
2020-04-27 06:46:33 -0300

    Update ex001.c

--- c/ex001.c
+++ c/ex001.c
@@ -2,11 +2,11 @@
 
 void main()
 {
-    printf("\n Listagem de Alunos");
-    printf("\n Nome\t\tNota");
-    printf("\n--------------------");
-    printf("\nVictoria F...\t10.0");
-    printf("\nSakuya Martins\t10.0");
-    printf("\nRemi Martins\t10.0");
-    printf("\nYuu Finotti\t10.0\n\n");
+  printf("\n Listagem de Alunos");
+  printf("\n Nome\t\tNota");
+  printf("\n--------------------");
+  printf("\nVictoria F...\t10.0");
+  printf("\nSakuya Martins\t10.0");
+  printf("\nRemi Martins\t10.0");
+  printf("\nYuu Finotti\t10.0\n\n");
 }
